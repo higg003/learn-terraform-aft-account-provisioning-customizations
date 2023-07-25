fa440dc03bb7402a6d297664865188dde6e6bc5e
Momcilo Dejanovic <momcilo.dejanovic.reyktech@sonymusic.com>
2023-07-25 10:36:19 +0200

    Chore - add Step Function name

--- terraform/iam.tf
+++ terraform/iam.tf
@@ -1,3 +1,7 @@
+###################################################################
+# Step Function - Account Provisioning Customizations
+###################################################################
+
 resource "aws_iam_role" "aft_states" {
   name               = "aft-account-provisioning-customizations-role"
   assume_role_policy = templatefile("${path.module}/iam/trust-policies/states.tpl", { none = "none" })
