431439b99750a83826a714c3883ed28822b4bb35
Momcilo Dejanovic <momcilo.dejanovic.reyktech@sonymusic.com>
2023-07-26 12:03:00 +0200

    Fix layer name

--- terraform/locals.tf
+++ terraform/locals.tf
@@ -1,6 +1,6 @@
 locals {
 
-  aft_layer_name             = "aft_common"
+  aft_layer_name             = "aft-common"
   aft_layer_suffix           = "1-9-0"
   aft_private_subnet_names   = ["aft-vpc-private-subnet-01", "aft-vpc-private-subnet-02"]
   aft_default_sg_name        = "aft-default-sg"
