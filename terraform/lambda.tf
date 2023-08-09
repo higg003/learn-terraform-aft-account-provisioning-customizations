33e32a508717ce30201ce4e5def6343e4b830f2a
Momcilo Dejanovic <momcilo.dejanovic.reyktech@sonymusic.com>
2023-08-09 14:20:01 +0200

    Fix Lambda names

--- terraform/lambda.tf
+++ terraform/lambda.tf
@@ -1,9 +1,9 @@
 resource "aws_lambda_function" "aft_add_alternative_contracts" {
   filename      = data.archive_file.aft_add_alternative_contracts.output_path
-  function_name = local.lambda_function_name
+  function_name = local.lambda_contracts_function_name
   description   = "Adds Alternate Contacts in the newly provisioned account"
   role          = aws_iam_role.aft_custom_provisioning.arn
-  handler       = "${local.lambda_function_name}.lambda_handler"
+  handler       = "${local.lambda_contracts_function_name}.lambda_handler"
 
   source_code_hash = data.archive_file.aft_add_alternative_contracts.output_base64sha256
   memory_size      = 1024
@@ -33,7 +33,7 @@ resource "aws_lambda_function" "aft_add_account_alias" {
   function_name = local.lambda_contracts_function_name
   description   = "Adds Account Alias in the newly provisioned account"
   role          = aws_iam_role.aft_custom_provisioning.arn
-  handler       = "${local.lambda_function_name}.lambda_handler"
+  handler       = "${local.lambda_contracts_function_name}.lambda_handler"
 
   source_code_hash = data.archive_file.aft_add_account_alias.output_base64sha256
   memory_size      = 1024
