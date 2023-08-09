77be5f36f5c63c8f1e0dc3c8b3991ba4037164ee
Momcilo Dejanovic <momcilo.dejanovic.reyktech@sonymusic.com>
2023-08-09 14:16:31 +0200

    Fix IAM

--- terraform/lambda.tf
+++ terraform/lambda.tf
@@ -2,7 +2,7 @@ resource "aws_lambda_function" "aft_add_alternative_contracts" {
   filename      = data.archive_file.aft_add_alternative_contracts.output_path
   function_name = local.lambda_function_name
   description   = "Adds Alternate Contacts in the newly provisioned account"
-  role          = aws_iam_role.aft_add_alternative_contracts_lambda.arn
+  role          = aws_iam_role.aft_custom_provisioning.arn
   handler       = "${local.lambda_function_name}.lambda_handler"
 
   source_code_hash = data.archive_file.aft_add_alternative_contracts.output_base64sha256
@@ -32,7 +32,7 @@ resource "aws_lambda_function" "aft_add_account_alias" {
   filename      = data.archive_file.aft_add_account_alias.output_path
   function_name = local.lambda_contracts_function_name
   description   = "Adds Account Alias in the newly provisioned account"
-  role          = aws_iam_role.aft_add_account_alias_lambda.arn
+  role          = aws_iam_role.aft_custom_provisioning.arn
   handler       = "${local.lambda_function_name}.lambda_handler"
 
   source_code_hash = data.archive_file.aft_add_account_alias.output_base64sha256
